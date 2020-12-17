c644fef21df0ac8bd3dce3a82c3d6fbbf72e9e39
vagrant <vagrant@localhost.localdomain>
2020-12-17 10:11:42 +0000

    [Finish] devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
