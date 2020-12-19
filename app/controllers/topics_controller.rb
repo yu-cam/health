4645834e5cfbe070ab06bf77e6674a97f6cf8017
vagrant <vagrant@localhost.localdomain>
2020-12-19 05:14:59 +0000

    [working]

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,44 @@
+class TopicsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+  	@topics = Topic.all
+    @new_topic = Topic.new
+  end
+
+  def show
+  	@topic = Topic.find(params[:id])
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+  	@new_topic = Topic.new(topic_params)
+  	@new_topic.save!
+  	redirect_to topic_path(@new_topic.id)
+  end
+
+  def edit
+  	@topic = Topic.find(params[:id])
+  end
+
+  def update
+  	@topic = Topic.find(params[:id])
+  	@topic.update(topic.params)
+  	redirect_to topic_path(@topic.id)
+  end
+
+  def destroy
+  	topic = Topic.find(params[:id])
+  	topic.destroy
+  	redirect_to topics_path
+  end
+
+  private
+  def topic_params
+  	params.require(:topic).permit(:title, :body)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_064847) do
+ActiveRecord::Schema.define(version: 2020_12_19_001153) do
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
